0190d31a0d16786223d92e6839bd79cf79ac48f8
mslnnkvlnd <mslnnkvlndx@gmail.com>
2022-09-10 15:09:46 +0300

    added 10 solutions

---
+++ Consecutive strings.js
@@ -0,0 +1,16 @@
+function longestConsec(strarr, k) {
+    if (strarr.length == 0 || k > strarr.length || k <= 0) return '';
+    
+    let longStr = '';
+    
+    let newStr = '';
+    
+    for (let i = 0; i < strarr.length; i++){
+      newStr = strarr.slice(i, i+k);
+      if (newStr.join('').length > longStr.length ){
+        longStr = newStr.join('');
+      }
+    }
+    
+    return longStr;
+}
